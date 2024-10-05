541410fa08d337491a80245141967d174bf0899d
guccho6w9 <gusta.quiroga033@gmail.com>
2024-10-05 18:26:36 -0300

    Add(-): actualizado navbar con futuros atajos a los bloques de la pagina

--- src/components/Navbar_menu.tsx
+++ src/components/Navbar_menu.tsx
@@ -1,25 +1,45 @@
 import { useState } from 'react';
 import '@/app/globals.css';
+
 const NavbarMenu = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
   
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
   
   return (
-    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <nav className="flex justify-between items-center p-2 mt-4 bg-black text-white mx-20">
       <ul className="flex space-x-4">
-        <li><a href="#hero" className="hover:underline">Inicio</a></li>
-        <li><a href="#about" className="hover:underline">Sobre mí</a></li>
-        <li><a href="#experience" className="hover:underline">Experiencia</a></li>
-        <li><a href="#projects" className="hover:underline">Proyectos</a></li>
-        <li><a href="#contact" className="hover:underline">Contacto</a></li>
+        <li>
+          <a href="#about" className="relative group">
+            <span className="hover:underline hover:text-teal-400 transition duration-300">Sobre mí</span>
+            <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-teal-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#experience" className="relative group">
+            <span className="hover:underline hover:text-teal-400 transition duration-300">Experiencia</span>
+            <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-teal-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#projects" className="relative group">
+            <span className="hover:underline hover:text-teal-400 transition duration-300">Proyectos</span>
+            <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-teal-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="relative group">
+            <span className="hover:underline hover:text-teal-400 transition duration-300">Contacto</span>
+            <span className="absolute left-0 right-0 bottom-0 h-0.5 bg-teal-400 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+          </a>
+        </li>
       </ul>
       
       <div className="flex items-center space-x-4">
-        <button onClick={toggleDarkMode}>
+        <button onClick={toggleDarkMode} className="hover:text-teal-400 transition duration-300">
           {isDarkMode ? "Modo Claro" : "Modo Oscuro"}
         </button>
-        <button className="hover:underline">EN / ES</button>
+        <button className="hover:text-teal-400 transition duration-300">EN / ES</button>
       </div>
     </nav>
   );
